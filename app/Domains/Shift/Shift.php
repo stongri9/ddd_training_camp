55fc296d833f4db35efe87296e66b29e9e7c3c8f
Minori Sato <minopi77@gmail.com>
2025-01-12 20:14:04 +0900

    シフト作成メソッドを定義しました

--- app/Domains/Shift/Shift.php
+++ app/Domains/Shift/Shift.php
@@ -27,4 +27,32 @@ class Shift extends DomainEntity {
         private array $nightShiftUserIds,
     ) {
     }
+
+    /**
+     * @param string $date
+     * @param int[] $dayShiftUserIds
+     * @param int[] $lateShiftUserIds
+     * @param int[] $nightShiftUserIds
+     */
+    public static function create(string $date, array $dayShiftUserIds, array $lateShiftUserIds, array $nightShiftUserIds) {
+        if (count($nightShiftUserIds) < 2) {
+            throw new \InvalidArgumentException('夜勤の人は2人以上必要です。');
+        }
+        $dateValueObject = new Date($date);
+
+        $shiftEntity = new Shift(null, $dateValueObject, $dayShiftUserIds, $lateShiftUserIds, $nightShiftUserIds);
+        
+        $dateTimeObject = new \DateTimeImmutable($dateValueObject->value);
+        $isBusinessDay = !in_array($dateTimeObject->format('D'), config('closedDays.closedWeekDays')) && !in_array($dateTimeObject->format('Y-m-d'), config('closedDays.holidays'));
+        if (!$isBusinessDay && count($dayShiftUserIds) < 3) {
+            throw new \InvalidArgumentException('休院日の場合、日勤の人は3人以上必要です。');
+        }
+        if ($isBusinessDay && count($dayShiftUserIds) < 4) {
+            throw new \InvalidArgumentException('営業日の場合、日勤の人は4人以上必要です。');
+        }
+        if ($isBusinessDay && count($lateShiftUserIds) < 1) {
+            throw new \InvalidArgumentException('営業日の場合、遅番の人は1人以上必要です。');
+        }
+        return $shiftEntity;
+    }
 }
