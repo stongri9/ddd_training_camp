c7e255e49dec11b1e8533be4f86a5e32832b3053
stongri9 <stongri9@gmail.com>
2025-03-15 15:16:49 +0900

    modify: 一覧画面のデザインを修正

--- resources/views/livewire/inquiry/show-inquiry.blade.php
+++ resources/views/livewire/inquiry/show-inquiry.blade.php
@@ -1,14 +1,32 @@
-<div>
+<div class="dark:text-white">
     @if ($inquiries->isNotEmpty())
-        @foreach ($inquiries as $inquiry)
-            <div wire:key="{{ $inquiry->id }}">id: {{ $inquiry->id }}</div>
-            <div>姓名：{{ $inquiry->last_name }} {{ $inquiry->first_name }}</div>
-            <div>電話番号：{{ $inquiry->tel }}</div>
-            <div>住所：{{ $inquiry->zip_code }} {{ $inquiry->address }}</div>
-            <div>問合せ内容：{{ $inquiry->content }}</div>
-            <livewire:inquiry.update-inquiry :id="$inquiry->id" />
-        @endforeach
+        <table class="w-full divide-y divide-gray-200 dark:divide-gray-800">
+            <thead class="bg-gray-50 dark:bg-gray-800">
+                <tr class="border-t border-b border-gray-200 dark:border-gray-700">
+                    <th scope="col" class="w-4 px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-300">ID</th>
+                    <th scope="col" class="w-48 px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-300">氏名</th>
+                    <th scope="col" class="w-6 px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-300">電話番号</th>
+                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-300">住所</th>
+                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-300">問合せ内容</th>
+                    <th scope="col" class="w-[8rem] px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-300">操作</th>
+                </tr>
+            </thead>
+            <tbody class="bg-white divide-y divide-gray-200 dark:bg-gray-800 dark:divide-gray-700">
+                @foreach ($inquiries as $inquiry)
+                    <tr wire:key="{{ $inquiry->id }}" class="border-b border-gray-200 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-900">
+                        <td class="px-6 py-4 whitespace-nowrap text-sm">{{ $inquiry->id }}</td>
+                        <td class="px-6 py-4 whitespace-nowrap text-sm">{{ $inquiry->last_name }} {{ $inquiry->first_name }}</td>
+                        <td class="px-6 py-4 whitespace-nowrap text-sm">{{ $inquiry->tel }}</td>
+                        <td class="px-6 py-4 text-sm">{{ $inquiry->zip_code }}<br>{{ $inquiry->address }}</td>
+                        <td class="px-6 py-4 text-sm">{{ $inquiry->content }}</td>
+                        <td class="px-6 py-4 whitespace-nowrap text-sm">
+                            <livewire:inquiry.update-inquiry :id="$inquiry->id" />
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
     @else
-        <div>データはありません！</div>
+        <div class="text-center py-4">データはありません！</div>
     @endif
 </div>
--- resources/views/pages/inquiry.blade.php
+++ resources/views/pages/inquiry.blade.php
@@ -4,17 +4,14 @@
             {{ __('Inquiry') }}
         </h2>
     </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mb-6 mx-auto sm:px-6 lg:px-8">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 py-12">
+    <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+    <div class="p-6 text-gray-900 dark:text-gray-100">
+        <div class="px-3 pb-3 flex justify-end">
             <livewire:inquiry.create-inquiry />
         </div>
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    <livewire:inquiry.show-inquiry />
-                </div>
-            </div>
-        </div>
+        <livewire:inquiry.show-inquiry />
+    </div>
+    </div>
     </div>
 </x-app-layout>
