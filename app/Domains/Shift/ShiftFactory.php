743d2b5b0fb7a90510ea3e6abd4390abd84fe3bb
naoki <naoki.tkhs.121031@gmail.com>
2025-03-15 16:29:02 +0900

    日勤、夕勤、夜勤を決める処理をprivateメソッドに分割

--- app/Domains/Shift/ShiftFactory.php
+++ app/Domains/Shift/ShiftFactory.php
@@ -6,52 +6,60 @@ use Illuminate\Support\Collection;
 
 class ShiftFactory {
 
-    public function create(DateTimeInterface $date, Collection $users, Shift $previousShift, Shift|null $confirmedNextShift){
-        //休み希望を出しているユーザーは勤務候補のユーザー（$users）から除外する
-        //前日夜勤のユーザーは休みにする必要があるので勤務候補のユーザー（$users）から除外する
+    public function create(DateTimeInterface $date, Collection $users, Shift $previousShift, Shift|null $confirmedNextShift) {
         $canWorkUsers = $users->filter(
             fn ($user) =>
                 !in_array($date, $user->dayOffRequests, true)
                 && !in_array($user->id, $previousShift->nightShiftUserIds, true)
         );
         
+        $workDayShiftUsers = $this->determineDayShiftUsers($date, $canWorkUsers);
+        $canWorkUsers = $canWorkUsers->reject(fn($user) => in_array($user->id, $workDayShiftUsers->pluck('id')->all(), true));
+        
+        $workLateShiftUsers = $this->determineLateShiftUsers($date, $canWorkUsers);
+        $canWorkUsers = $canWorkUsers->reject(fn($user) => in_array($user->id, $workLateShiftUsers->pluck('id')->all(), true));
+        
+        $workNightShiftUsers = $this->determineNightShiftUsers($date, $canWorkUsers, $confirmedNextShift);
+        
+        return Shift::create(
+            $date->format('Y-m-d'), 
+            $workDayShiftUsers->pluck('id')->all(), 
+            $workLateShiftUsers->pluck('id')->all(), 
+            $workNightShiftUsers->pluck('id')->all()
+        );
+    }
+
+    private function determineDayShiftUsers(DateTimeInterface $date, Collection $canWorkUsers): Collection {
         $numberOfDayShiftUser = $this->getNumberOfShiftUser($date, 'day');
-        // 日勤をきめる
-        $canWorkDayShiftUsers = $canWorkUsers->filter(fn ($user) => $user->role !== 'arbeit');
         $canWorkNurseOrAssociateNurseUsers = $canWorkUsers->filter(
-            fn ($user) => $user->role === 'associateNurse' || $user->role === 'nurse'
+            fn ($user) => in_array($user->role, ['associateNurse', 'nurse'], true)
         );
         $workDayShiftNurseOrAssociateNurseUser = $canWorkNurseOrAssociateNurseUsers->random();
-        //日勤は、働ける看護師・准看護師から1人と、残りは働ける人たち（先述した1人を除外した）で埋める
-        $workDayShiftUsers = $workDayShiftNurseOrAssociateNurseUser
-            ->add(
-                $canWorkDayShiftUsers
-                    ->reject(fn($user)=> $user->id === $workDayShiftNurseOrAssociateNurseUser->id)
-                    ->random($numberOfDayShiftUser - 1)
-            );
-        //日勤で選ばれたユーザーはこのあとの遅番・夜勤の候補となる$canWorkUsersから除外しておく
-        $canWorkUsers = $canWorkUsers->reject(
-            fn($user) 
-                => in_array($user->id, $workDayShiftUsers->pluck('id')->all(), true));
+        
+        return $workDayShiftNurseOrAssociateNurseUser->add(
+            $canWorkUsers
+                ->reject(fn($user) => $user->id === $workDayShiftNurseOrAssociateNurseUser->id)
+                ->random($numberOfDayShiftUser - 1)
+        );
+    }
 
-        // 遅番をきめる
+    private function determineLateShiftUsers(DateTimeInterface $date, Collection $canWorkUsers): Collection {
         $numberOfLateShiftUser = $this->getNumberOfShiftUser($date, 'late');
-        $workLateShiftUsers = new Collection();
-        if ($numberOfDayShiftUser > 0) {
-            $workLateShiftUsers = $canWorkUsers
-                ->filter(fn($user) => $user->role === 'associateNurse' || $user->role === 'nurse')
+        if ($numberOfLateShiftUser > 0) {
+            return $canWorkUsers
+                ->filter(fn($user) => in_array($user->role, ['associateNurse', 'nurse'], true))
                 ->random($numberOfLateShiftUser);
-            //遅番で選ばれたユーザーはこのあとの夜勤の候補となる$canWorkUsersから除外しておく
-            $canWorkUsers = $canWorkUsers->reject(fn($user)=> $user->id === $workLateShiftUsers->id);
         }
+        return new Collection();
+    }
 
-        // 夜勤をきめる
+    private function determineNightShiftUsers(DateTimeInterface $date, Collection $canWorkUsers, ?Shift $confirmedNextShift): Collection {
         $numberOfNightShiftUser = $this->getNumberOfShiftUser($date, 'night');
         $canWorkNightShiftUsers = $canWorkUsers->reject(
             fn($user) => in_array($user->role, ['headNurse', 'chief', 'part'], true)
         );
-        //翌日の勤務予定のユーザーを夜勤の候補から除外する
-        if (isset($confirmedNextShift)){
+        
+        if (isset($confirmedNextShift)) {
             $canWorkNightShiftUsers = $canWorkNightShiftUsers->reject(
                 fn ($user) => in_array(
                     $user->id, 
@@ -64,27 +72,19 @@ class ShiftFactory {
                 )
             );
         }
+        
         $workNightShiftNurseUser = $canWorkNightShiftUsers
             ->filter(fn ($user) => $user->role === 'nurse')
             ->random();
-        //夜勤は、働ける看護師から1人と、残りは働ける人たち（先述した1人を除外した）で埋める
-        $workNightShiftUsers = $workNightShiftNurseUser
-            ->add(
-                $canWorkNightShiftUsers
-                    ->reject(fn($user) => $user->id === $workNightShiftNurseUser->id)
-                    ->random($numberOfNightShiftUser - 1)
-            );
-
-        return Shift::create(
-            $date->format('Y-m-d'), 
-            $workDayShiftUsers->pluck('id')->all(), 
-            $workLateShiftUsers->pluck('id')->all(), 
-            $workNightShiftUsers->pluck('id')->all()
+        
+        return $workNightShiftNurseUser->add(
+            $canWorkNightShiftUsers
+                ->reject(fn($user) => $user->id === $workNightShiftNurseUser->id)
+                ->random($numberOfNightShiftUser - 1)
         );
     }
 
-    private function getNumberOfShiftUser(DateTimeInterface $date, string $workStyle): int
-    {
+    private function getNumberOfShiftUser(DateTimeInterface $date, string $workStyle): int {
         $isBusinessDay = 
             !in_array(($date)->format('D'), config('closedDays.closedWeekDays'), true)
             && !in_array(($date)->format('Y-m-d'), config('closedDays.holidays'), true);
